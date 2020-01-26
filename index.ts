2df7fc8152768113360f3c14055e9de3d86ae089
francyscasale92 <francesca.casale92@gmail.com>
2020-01-26 12:45:02 +0100

    pipe()

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import { of } from 'rxjs'; 
 import { fromEvent } from 'rxjs'; 
 import { paint } from './canvas.js';
-import { mergeMap, takeUntil } from 'rxjs/operators';
+import { mergeMap, takeUntil, delay } from 'rxjs/operators';
  
 
-const mouse$ = fromEvent(document, 'mousemove');
-mouse$.subscribe(console.log);
+// const mouse$ = fromEvent(document, 'mousemove');
+// mouse$.subscribe(console.log);
 
-// const move$ = fromEvent(document, 'mousemove');
+const move$ = fromEvent(document, 'mousemove').pipe(delay(2000));
 // const down$ = fromEvent(document, 'mousedown')
 // const up$ = fromEvent(document, 'mouseup')
 
@@ -16,3 +16,5 @@ mouse$.subscribe(console.log);
 // );
 
 // paints$.subscribe((v)=> paint(v));
+
+move$.subscribe((v)=> paint(v));
