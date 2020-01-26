1c8158b4087727901d6548d5bd6d20fab6898ece
francyscasale92 <francesca.casale92@gmail.com>
2020-01-26 13:36:24 +0100

    fixed comments

--- index.ts
+++ index.ts
@@ -2,26 +2,33 @@ import { of } from 'rxjs';
 import { fromEvent } from 'rxjs'; 
 import { paint } from './canvas.js';
 import { delay, debounceTime, tap } from 'rxjs/operators';
+// BONUS
 //import { mergeMap, takeUntil, delay } from 'rxjs/operators';
  
+// TEST 1
+const mouse$ = fromEvent(document, 'mousemove');
+mouse$.subscribe(console.log);
 
-// const mouse$ = fromEvent(document, 'mousemove');
-// mouse$.subscribe(console.log);
+// TEST 2
+// const move$ = fromEvent(document, 'mousemove');
+// move$.subscribe((v)=> paint(v));
 
-//const move$ = fromEvent(document, 'mousemove').pipe(delay(2000));
-const move$ = fromEvent(document, 'mousemove').pipe(
-  //debounceTime(1000),
-  tap(paint)
-);
+// TEST 3
+// const move$ = fromEvent(document, 'mousemove').pipe(delay(2000));
+// move$.subscribe((v)=> paint(v));
 
+// TEST 4
+// const move$ = fromEvent(document, 'mousemove').pipe(debounceTime(1000));
+// move$.subscribe((v)=> paint(v));
+
+// TEST 5
+// const move$ = fromEvent(document, 'mousemove').pipe(tap(paint));
+// move$.subscribe();
+
+// BONUS
 // const down$ = fromEvent(document, 'mousedown')
 // const up$ = fromEvent(document, 'mouseup')
-
 // const paints$ = down$.pipe(
 //   mergeMap(down => move$.pipe(takeUntil(up$)))
 // );
-
 // paints$.subscribe((v)=> paint(v));
-
-//move$.subscribe((v)=> paint(v));
-move$.subscribe();
